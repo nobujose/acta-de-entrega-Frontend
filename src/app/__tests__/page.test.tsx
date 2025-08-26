5cc6316f69df1d1abc14cca156d876e6af4cf265
joseA <nobujose6@gmail.com>
2025-08-26 13:22:04 -0400

    fix: Solucionado conflicto de Tailwind y errores de visualización en login

--- src/app/__tests__/page.test.tsx
+++ src/app/__tests__/page.test.tsx
@@ -1,12 +1,19 @@
-import { render, screen } from '@testing-library/react';
 import Home from '../page';
+import { redirect } from 'next/navigation';
+
+// Simular (mock) el módulo 'next/navigation'
+jest.mock('next/navigation', () => ({
+  redirect: jest.fn(),
+}));
 
 describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />);
+  it('should call redirect with "/login"', () => {
+    // Llamar a la función del componente directamente en lugar de usar render
+    Home();
 
-    const heading = screen.getByText(/Get started by editing/i);
-
-    expect(heading).toBeInTheDocument();
+    // Verificar que la función redirect fue llamada
+    expect(redirect).toHaveBeenCalledWith('/login');
+    // Opcional: Verificar que fue llamada solo una vez
+    expect(redirect).toHaveBeenCalledTimes(1);
   });
 });
