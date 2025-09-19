f359bd917102077695ab129483ad77e127a91c9b
joseA <nobujose6@gmail.com>
2025-09-19 15:32:37 -0400

    mayusculas

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 // src/components/Sidebar.tsx
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 import { cn } from '@/lib/utils';
